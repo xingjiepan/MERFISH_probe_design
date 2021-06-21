44e8d00ed3d7e5a0b13dbd6e353cff5b6086b177
Xingjie Pan <xingjiepan@gmail.com>
2021-06-21 11:03:24 -0400

    Re-organize the file filters.py

--- MERFISH_probe_design/probe_design/filters.py
+++ MERFISH_probe_design/probe_design/filters.py
@@ -6,6 +6,18 @@ from Bio.SeqUtils import GC
 from Bio.SeqUtils import MeltingTemp
 
 
+def filter_probe_dict_by_metric(probe_dict:pd.core.frame.DataFrame, column_key:str, 
+        lower_bound:float=-np.Inf, upper_bound:float=np.Inf):
+    '''Filter the probe dictionary by a metric.'''
+    for gk in probe_dict.keys():
+        print(gk)
+        for tk in probe_dict[gk].keys():
+            new_df = probe_dict[gk][tk][probe_dict[gk][tk][column_key].between(
+                lower_bound, upper_bound, inclusive=False)]
+            
+            print(f'\t{tk}: {new_df.shape[0]} / {probe_dict[gk][tk].shape[0]} probes passed the filter {lower_bound} < {column_key} <  {upper_bound}.')
+            probe_dict[gk][tk] = new_df
+
 def calc_gc_for_probe_dict(probe_dict:pd.core.frame.DataFrame, 
         column_key_seq:str='target_sequence', column_key_write='target_GC'):
     '''Calculate GC content of sequences under the column_key_seq column in the probe dictionary.
@@ -39,17 +51,4 @@ def calc_tm_for_probe_dict(probe_dict:pd.core.frame.DataFrame, Na_conc:float, fm
                 tms.append(MeltingTemp.chem_correction(tm_raw, fmd=fmd_percentile))
 
             probe_dict[gk][tk][column_key_write] = pd.Series(tms, index=probe_dict[gk][tk].index)
-
-def filter_probe_dict_by_metric(probe_dict:pd.core.frame.DataFrame, column_key:str, 
-        lower_bound:float=-np.Inf, upper_bound:float=np.Inf):
-    '''Filter the probe dictionary by a metric.'''
-    for gk in probe_dict.keys():
-        print(gk)
-        for tk in probe_dict[gk].keys():
-            new_df = probe_dict[gk][tk][probe_dict[gk][tk][column_key].between(
-                lower_bound, upper_bound, inclusive=False)]
-            
-            print(f'\t{tk}: {new_df.shape[0]} / {probe_dict[gk][tk].shape[0]} probes passed the filter {lower_bound} < {column_key} <  {upper_bound}.')
-            probe_dict[gk][tk] = new_df
-
 
