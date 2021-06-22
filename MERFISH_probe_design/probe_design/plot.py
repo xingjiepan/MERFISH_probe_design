b73893b065cae27c71c74762b7e748aea5d1441f
Xingjie Pan <xingjiepan@gmail.com>
2021-06-22 10:15:55 -0400

    Implement the plot_sequence_coverage() function

--- MERFISH_probe_design/probe_design/plot.py
+++ MERFISH_probe_design/probe_design/plot.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def get_values_from_probe_dict(probe_dict:pd.core.frame.DataFrame, column_key:str):
+def get_values_from_probe_dict(probe_dict:dict, column_key:str):
     '''Get values under the column_key from a probe dictionary.
     Return a list of values.
     '''
@@ -15,7 +16,7 @@ def get_values_from_probe_dict(probe_dict:pd.core.frame.DataFrame, column_key:st
     
     return values
 
-def plot_hist(probe_dict:pd.core.frame.DataFrame, column_key:str, y_max=None, bins=30):
+def plot_hist(probe_dict:dict, column_key:str, y_max=None, bins=30):
     '''Plot histogram of values under the column_key'''
     plt.hist(get_values_from_probe_dict(probe_dict, column_key), bins=bins)
 
@@ -25,5 +26,22 @@ def plot_hist(probe_dict:pd.core.frame.DataFrame, column_key:str, y_max=None, bi
 
     plt.xlabel(column_key)
     plt.ylabel('Count')
+    plt.show()
+
+def plot_sequence_coverage(df:pd.core.frame.DataFrame, seq_length:int):
+    '''Plot the sequence coverage of a sequences.'''
+    coverage = np.zeros(seq_length, dtype=int)
+
+    shifts = list(df['shift'])
+    target_seqs = list(df['target_sequence'])
+
+    for i in range(len(shifts)):
+        shift = shifts[i]
+        t_len = len(target_seqs[i])
+        coverage[shift : shift + t_len] += 1
+
+    plt.plot(np.arange(seq_length), coverage)
+    plt.xlabel('Sequence position')
+    plt.ylabel('Coverage')
     plt.show()
 
