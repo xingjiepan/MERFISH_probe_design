3d16fac7e3efe86ed917b238ea54b266f7b76a74
Xingjie Pan <xingjiepan@gmail.com>
2021-06-23 11:47:13 -0400

    Implement the generate_transcript_level_report() function

--- MERFISH_probe_design/probe_design/quality_check.py
+++ MERFISH_probe_design/probe_design/quality_check.py
@@ -30,21 +30,55 @@ def check_and_standardize_transcriptome(transcriptome:pd.core.frame.DataFrame,
     
     return standard_transcriptome
 
+def barcode_str_to_array(bc_str:str):
+    return np.array([int(c) for c in bc_str])
+
+def barcode_array_to_str(bc_array:np.ndarray):
+    return ''.join(['1' if i > 0 else '0' for i in bc_array])
+
+def coverage_string(probe_bit_counts:np.ndarray):
+    return ':'.join([str(i) for i in probe_bit_counts if i > 0])
+
+def max_N_non_overlapping_probes(shifts:list, target_length:int):
+    ss = sorted(shifts)
+    N_accepted = 0
+    tail = -1
+
+    for s in ss:
+        if s > tail:
+            N_accepted += 1
+            tail = s + target_length - 1
+
+    return N_accepted
+
 def generate_transcript_level_report(probe_dict:dict, transcriptome:pd.core.frame.DataFrame):
     '''Generate a data frame of transcript level metrics.
     '''
     metrics = {'gene_id':[], 'gene_short_name':[], 'transcript_id':[], 'FPKM':[],
-            'length':[], 'N_probes':[]}
+            'length':[], 'barcode':[], 'N_probes':[], 'probe_bit_coverage':[],
+            'max_N_non_overlapping_probes':[]}
 
     for gk in probe_dict.keys():
         for tk in probe_dict[gk].keys():
             transcript_df = transcriptome[transcriptome['transcript_id'] == tk]
-            
+
+            # Add the basic metrics
             metrics['gene_id'].append(transcript_df.iloc[0]['gene_id'])
             metrics['gene_short_name'].append(transcript_df.iloc[0]['gene_short_name'])
             metrics['transcript_id'].append(transcript_df.iloc[0]['transcript_id'])
             metrics['FPKM'].append(transcript_df.iloc[0]['FPKM'])
             metrics['length'].append(len(transcript_df.iloc[0]['sequence']))
             metrics['N_probes'].append(probe_dict[gk][tk].shape[0])
+
+            # Calculate barcode related metrics
+            probe_barcodes = [barcode_str_to_array(bc_str) for bc_str in probe_dict[gk][tk]['probe_barcode']]
+            probe_bit_counts = np.sum(probe_barcodes, axis=0)
+            metrics['barcode'].append(barcode_array_to_str(probe_bit_counts))
+            metrics['probe_bit_coverage'].append(coverage_string(probe_bit_counts))
+
+            # Calculate the overlapping matric
+            target_length = len(probe_dict[gk][tk].iloc[0]['target_sequence']) 
+            metrics['max_N_non_overlapping_probes'].append(max_N_non_overlapping_probes(
+                list(probe_dict[gk][tk]['shift']), target_length))
 
     return pd.DataFrame(metrics)
