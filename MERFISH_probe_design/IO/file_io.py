4baf9afb3f5970c19943eed6521ae108f7618a19
zhengpuas47 <zhengpuas47@gmail.com>
2023-02-16 19:12:03 -0500

    update fio

--- MERFISH_probe_design/IO/file_io.py
+++ MERFISH_probe_design/IO/file_io.py
@@ -95,8 +95,23 @@ def load_transcriptome(transcripts_fasta_file:str, fpkm_tracking_file:str=None):
    
     else:
         transcripts['FPKM'] = [1] * transcripts.shape[0]
-        transcripts['gene_id'] = pd.Series(dtype=str)
-        transcripts['gene_short_name'] = pd.Series(dtype=str)
+        # try to parse description column, if its ensembl file:
+        gene_id_list, gene_name_list = [], []
+        print(len(transcripts))
+        for _str in transcripts['description']:
+            _gene_id_match = re.search('gene:([a-zA-Z0-9\.]+) ', _str)
+            if _gene_id_match:
+                gene_id_list.append(_gene_id_match.groups()[0])
+            else:
+                gene_id_list.append(None)
+            _gene_name_match = re.search('gene_symbol:([a-zA-Z0-9\.]+) ', _str)
+            if _gene_name_match:
+                gene_name_list.append(_gene_name_match.groups()[0])
+            else:
+                gene_name_list.append(None)
+
+        transcripts['gene_id'] = pd.Series(gene_id_list, dtype=str)
+        transcripts['gene_short_name'] = pd.Series(gene_name_list, dtype=str)
         transcriptome = transcripts
 
     return transcriptome
